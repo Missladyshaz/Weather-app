1b571396cdbd5fc3049ea13cffe707b3219a0da1
Missladyshaz <101677084+Missladyshaz@users.noreply.github.com>
2022-04-25 22:59:21 +1000

    fixed small issues with pulling data
    lost some data when moving over, now fixed

--- java.js
+++ java.js
@@ -1,43 +1,36 @@
-//1.In your project, display the current date and time using JavaScript: Tuesday 16:00
-let h5 = document.querySelector("h6");
-
-let now = new Date();
-let hours = now.getHours();
-let minutes = now.getMinutes();
-let year = now.getFullYear();
-let days = [
-  "Sunday",
-  "Monday",
-  "Tuesday",
-  "Wenesday",
-  "Thursday",
-  "Friday",
-  "Saturday",
-];
-let day = days[now.getDay()];
-let months = [
-  "Jan",
-  "Feb",
-  "March",
-  "April",
-  "May",
-  "June",
-  "July",
-  "Aug",
-  "Sep",
-  "Oct",
-  "Nov",
-  "Dec",
-];
-let month = months[now.getMonth()];
-
-h5.innerHTML = `${hours} : ${minutes} ${now}`;
 //2.Add a search engine, when searching for a city (i.e. Paris), display the city name on the page after the user submits the form.
+function formatDate(date) {
+  let hours = date.getHours();
+  if (hours < 10) {
+    hours = `0${hours}`;
+  }
+  let minutes = date.getMinutes();
+  if (minutes < 10) {
+    minutes = `0${minutes}`;
+  }
 
+  let dayIndex = date.getDay();
+  let days = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+  ];
+  let day = days[dayIndex];
+
+  return `${day} ${hours}:${minutes}`;
+}
 // app
 function showTemperature(response) {
   document.querySelector("#displayCity").innerHTML = response.data.name;
-  console.log(response.data.name);
+  document.querySelector("#displayTemperature").innerHTML = Math.round(
+    response.data.main.temp
+  );
+  document.querySelector("#humidity").innerHTML = response.data.main.humidity;
+  document.querySelector("#wind").innerHTML = response.data.wind.speed;
 }
 
 function searchCity(event) {
@@ -47,3 +40,6 @@ function searchCity(event) {
   let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
   axios.get(apiUrl).then(showTemperature);
 }
+
+let form = document.querySelector("form");
+form.addEventListener("submit", searchCity);
